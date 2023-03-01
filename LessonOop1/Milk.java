3752effd95627c674b5a25a31457d9eaffb29a5e
olesya <olesya_khvostova@mail.ru>
2023-03-01 21:58:50 +0300

    Add Lesson1

---
+++ LessonOop1/Milk.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+
+public class Milk extends Product {
+    private Date expireDate = new Date();
+    private String producer = new String();
+    public Milk(String producer, double cost, Date expireDate) {
+        super("Молоко", cost);
+        this.producer = producer;
+        this.expireDate = expireDate;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder localString = new StringBuilder(super.toString());
+        localString.append(String.format("Молоко годно до %s Производитель %s", expireDate.toString(), producer));
+        return localString.toString();
+    }
+}
